e54843d4ea55a191816e379361aafe4bfcba3297
otienomaurice1 <134706372+otienomaurice1@users.noreply.github.com>
2023-12-08 12:32:38 -0500

    Update pr_m_w.sv

--- lab11/sv/pr_m_w.sv
+++ lab11/sv/pr_m_w.sv
@@ -2,9 +2,13 @@ module pr_m_w(input logic clk, reset,
   input logic regwrite_m, memtoreg_m,
   input logic [31:0] aluout_m, readdata_m,
   input logic [4:0] writereg_m,
+  input logic jal_m,
+  input logic [31:0] pcplus4_m,
   output logic regwrite_w, memtoreg_w,
   output logic [31:0] aluout_w, readdata_w,
-  output logic [4:0] writereg_w
+  output logic [4:0] writereg_w,
+  output logic [31:0]  pcplus4_w,
+  output logic jal_w
   );
 
   always_ff @ (posedge clk)
@@ -14,6 +18,8 @@ module pr_m_w(input logic clk, reset,
     aluout_w <= '0;
     readdata_w <= '0;
     writereg_w <= '0;
+    jal_w <= '0;
+    pcplus4_w <= '0;
   end
   else begin
     regwrite_w <= regwrite_m;
@@ -21,5 +27,7 @@ module pr_m_w(input logic clk, reset,
     aluout_w <= aluout_m;
     readdata_w <= readdata_m;
     writereg_w <= writereg_m;
+    pcplus4_w <= pcplus4_m;
+    jal_w <= jal_m; //added the jump and link signal from memory stage
   end
 endmodule: pr_m_w
